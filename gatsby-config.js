1579e1dbb5598c0d3e139fd52a10c103171ea5b9
Owais <precisework01@gmail.com>
2021-04-18 02:12:44 +0500

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,12 +54,12 @@ module.exports = {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
-          apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-          authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-          projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-          storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.REACT_APP_FIREBASE_APP_ID
+          apiKey: `AIzaSyD-3y2K7hgHs74ewp-k7MxchCoTUMw7bCo`,
+          authDomain: `auth-demo-f9904.firebaseapp.com`,
+          projectId: `auth-demo-f9904`,
+          storageBucket: `auth-demo-f9904.appspot.com`,
+          messagingSenderId: `990919786702`,
+          appId: `1:990919786702:web:64fe2ecbd8b32f4095d09f`
         }
       }
     }
